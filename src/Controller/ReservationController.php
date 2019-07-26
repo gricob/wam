e01531c84fc5699726a845527fa0e7cd569b8e1f
Gerardo Rico Botella <gerardo.rico@multimediacreations.eu>
2019-07-26 18:47:15 +0200

    Separa la vista del controlador

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -3,36 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ReservationController
+class ReservationController extends AbstractController
 {
     public function index()
     {
         $raw = fopen($_ENV['RESERVATIONS_CSV'], 'r');
 
-        $html = 
-        '<table>
-            <tbody>';
-
+        $reservations = [];
         while(($reservation = fgetcsv($raw, 0, ';')) !== false) {
             if(count($reservation) == 7) {
-                $html .= 
-                "<tr id='reservation-$reservation[0]' class='reservation'>
-                <td class='id'>$reservation[0]</td>
-                <td class='guest'>$reservation[1]</td>
-                <td class='checkin'>$reservation[2]</td>
-                <td class='checkout'>$reservation[3]</td>
-                <td class='hotel'>$reservation[4]</td>
-                <td class='amount'>$reservation[5]</td>
-                <td class='actions'>$reservation[6]</td>
-                </tr>";
+                $reservations[] = $reservation;
             }
         }
 
-        $html .= 
-            '</tbody>
-        </table>';
-
-        return new Response($html);
+        return $this->render('reservations/index.html.twig', [
+            'reservations' => $reservations
+        ]);
     }
 }
